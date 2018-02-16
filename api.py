40a81c0004b04d467678276d7f418ac6d6f35820
Adrián Vázquez <a.vazquezrd@gmail.com>
2018-02-17 00:35:33 +0100

    Conf. básica / Añadido getBocadillos (error al serializar tipo Decimal)

--- api.py
+++ api.py
@@ -1,10 +1,36 @@
 from flask import Flask
+from flask_restful import Resource, Api
+from sqlalchemy import create_engine
+import logging
+import sys
 
+URI = open('.mainudb', 'r').read()
+LOG_PATH = 'log/mainu_api.log'
+STDOUT_PATH = 'log/.trash'
+STDERR_PATH = 'log/error.log'
+
+sys.stdout = open(STDOUT_PATH, 'a')
+sys.stderr = open(STDERR_PATH, 'a')
+logging.basicConfig(filename=LOG_PATH,
+                    format='%(asctime)s - %(funcName)s - ' +
+                    '%(levelname)s - %(message)s',
+                    filemode='a',
+                    level=logging.DEBUG)
+
+db_connect = create_engine(URI)
 app = Flask(__name__)
+api = Api(app)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+
+class Bocadillos(Resource):
+    def get(self):
+        logging.info("Devuelve lista de bocadillos")
+        conn = db_connect.connect()
+        query = conn.execute("SELECT * FROM Bocadillo")
+        return {'Bocadillos': [i for i in query.cursor.fetchall()]}
+
+
+api.add_resource(Bocadillos, '/getBocadillos')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=5002)
