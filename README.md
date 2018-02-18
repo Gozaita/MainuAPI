a0e6af54e6e13410b389e1292b37c5414ba1e721
Adrián Vázquez <a.vazquezrd@gmail.com>
2018-02-18 11:44:58 +0100

    Reducidas dependencias / Log mejorado / Código mejorado

--- README.md
+++ README.md
@@ -23,12 +23,30 @@ El proyecto de MainU
 
 Dependencias
 -------------
+
 ```
-pip3 install sqlalchemy mysqlclient flask flask-jsonpify flask-restful simplejson
+pip3 install sqlalchemy
+pip3 install mysqlclient
+pip3 install flask
+pip3 install simplejson
 ```
 La instalación de SQLAlchemy puede dar problemas en entornos Windows. Si se da el caso, descargar de la [web oficial](https://www.sqlalchemy.org/download.html), descomprimir e instalar usando `py setup.py install`.
 
-API
+Funciones ofrecidas por la API
 -------------
 
+La API REST que proporciona acceso a las diferentes funciones que aquí se muestran está accesible a través de **mainu.eus/api**. Junto a cada función se indican los métodos soportados (`GET`, `POST`).
+
+- `/get_bocadillos` `[GET]`
+
+Devuelve un `array` en formato `JSON`, donde cada elemento tiene el siguiente formato:
+```
+{
+  "calorias": null,
+  "id": 1,
+  "izena": "Ave César",
+  "nombre": "Ave César",
+  "precio": 3.50
+}
+```
 > *En desarrollo...*
--- api.py
+++ api.py
@@ -1,38 +1,56 @@
-from flask import Flask
-from flask_restful import Resource, Api
+from flask import Flask, jsonify
 from sqlalchemy import create_engine
-from flask_jsonpify import jsonify
-import logging
+from time import localtime, strftime
+import os
 import sys
+import logging
 
 URI = open('.mainudb', 'r').read()
-LOG_PATH = 'log/mainu_api.log'
-STDOUT_PATH = 'log/.trash'
-STDERR_PATH = 'log/error.log'
+LOG_PATH = 'log/mainu.log'
 
-sys.stdout = open(STDOUT_PATH, 'a')
-sys.stderr = open(STDERR_PATH, 'a')
+try:
+    log = open(LOG_PATH, 'a')
+except FileNotFoundError:
+    if not os.path.exists(os.path.dirname(LOG_PATH)):
+        os.makedirs(os.path.dirname(LOG_PATH))
+    log = open(LOG_PATH, 'w')
+
+log.write("##############################################\n")
+log.write("MainU -- mainu.eus -- %s\n"
+          % strftime("%Y-%m-%d %H:%M:%S", localtime()))
+log.write("API REST\n")
+log.write("##############################################\n")
+log.close()
+
+sys.stdout = open(LOG_PATH, 'a')
+sys.stderr = sys.stdout
+
 logging.basicConfig(filename=LOG_PATH,
-                    format='%(asctime)s - %(funcName)s - ' +
-                    '%(levelname)s - %(message)s',
+                    format='[%(asctime)s]: %(levelname)s - ' +
+                    '%(funcName)s - %(message)s',
                     filemode='a',
                     level=logging.DEBUG)
 
-db_connect = create_engine(URI)
+db = create_engine(URI)
 app = Flask(__name__)
-api = Api(app)
+
+#############################################
+# MainU API
+#############################################
 
 
-class Bocadillos(Resource):
-    def get(self):
-        logging.info("Devuelve lista de bocadillos")
-        conn = db_connect.connect()
-        query = conn.execute("SELECT * FROM Bocadillo")
-        return jsonify({'Bocadillos': [dict(zip(query.keys(), i))
-                                       for i in query.cursor.fetchall()]})
+@app.route("/get_bocadillos", methods=["GET"])
+def get_bocadillos():
+    logging.info("Devolviendo lista de bocadillos...")
+    try:
+        cx = db.connect()
+        query = cx.execute("SELECT * FROM Bocadillo")
+        return jsonify([dict(zip(query.keys(), i))
+                       for i in query.cursor.fetchall()])
+    except Exception as e:
+        logging.exception("No se ha podido realizar la petición")
+        return None
 
-
-api.add_resource(Bocadillos, '/getBocadillos')
 
 if __name__ == '__main__':
-    app.run(port=5002)
+    app.run(port=80)
