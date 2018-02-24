2df037a742b20dfb50c10ae40b863b04b06c268d
Adrián Vázquez <a.vazquezrd@gmail.com>
2018-02-24 14:11:35 +0100

    Actualizado get_menu / Mejorado get_bocadillos

--- api.py
+++ api.py
@@ -9,6 +9,12 @@ import logging
 URI = open('.mainudb', 'r').read()
 LOG_PATH = 'log/mainu.log'
 
+SRV_PATH = 'https://www.mainu.eus/'
+IMG_PATH = SRV_PATH + 'external/images/'
+BOC_PATH = IMG_PATH + 'bocadillos/'
+PLT_PATH = IMG_PATH + 'platos/'
+OTH_PATH = IMG_PATH + 'otros/'
+
 try:
     log = open(LOG_PATH, 'a')
 except FileNotFoundError:
@@ -48,16 +54,16 @@ def get_bocadillos():
         bocs = cx.execute("SELECT * FROM Bocadillo")
         bocs_final = []
         for b in bocs:
-            ing_ids = cx.execute("SELECT * FROM IngredienteBocadillo " +
+            ings_id = cx.execute("SELECT * FROM IngredienteBocadillo " +
                                  "WHERE Bocadillo_id=%d" % b['id'])
-            ing = []
-            for i in ing_ids:
-                ing_lst = cx.execute("SELECT * FROM Ingrediente WHERE " +
-                                     "Ingrediente.id=%d" % i['Ingrediente_id'])
-                for i in ing_lst:
-                    ing.append(i['nombre'])
+            ings = []
+            for i in ings_id:
+                ing = cx.execute("SELECT Ingrediente.nombre FROM " +
+                                 "Ingrediente WHERE Ingrediente.id=%d"
+                                 % i['Ingrediente_id']).fetchone()
+                ings.append(ing['nombre'])
             boc = {'id': b['id'], 'nombre': b['nombre'], 'precio': b['precio'],
-                   'puntuacion': b['puntuacion'], 'ingredientes': ing}
+                   'puntuacion': b['puntuacion'], 'ingredientes': ings}
             bocs_final.append(boc)
         return jsonify(bocs_final)
     except Exception as e:
@@ -65,37 +71,31 @@ def get_bocadillos():
         return None
 
 
-def fetch_menu():
-        cx = db.connect()
-        return cx.execute("SELECT * FROM Plato WHERE actual=True")
-
-
 @app.route("/get_menu", methods=["GET"])
 def get_menu():
     logging.info("Devolviendo menú del día...")
     try:
-        query = fetch_menu()
-        return jsonify([dict(zip(query.keys(), i))
-                       for i in query.cursor.fetchall()])
-    except Exception as e:
-        logging.exception("No se ha podido realizar la petición")
-        return None
-
-
-@app.route("/get_menu_summary", methods=["GET"])
-def get_menu_summary():
-    logging.info("Devolviendo primeros del menú del día...")
-    try:
-        menu = fetch_menu()
+        cx = db.connect()
+        menu = cx.execute("SELECT * FROM Plato WHERE actual=True")
         pr = []
         sg = []
         for p in menu:
-            if p['tipo'] == 1:
-                pr.append(p['nombre'])
-            elif p['tipo'] == 2:
-                sg.append(p['nombre'])
+            logging.debug("Plato %d" % p['id'])
+            im = cx.execute("SELECT FotoPlato.ruta FROM FotoPlato WHERE " +
+                            "FotoPlato.Plato_id=%d AND " % p['id'] +
+                            "FotoPlato.oficial=True").fetchone()
+            if im is not None:
+                img = PLT_PATH + im['ruta']
             else:
-                ps = p['nombre']
+                img = None
+            pl = {'id': p['id'], 'nombre': p['nombre'],
+                  'puntuacion': p['puntuacion'], 'imagen': img}
+            if p['tipo'] == 1:
+                pr.append(pl)
+            elif p['tipo'] == 2:
+                sg.append(pl)
+            else:
+                ps = pl
         return jsonify({'primeros': pr, 'segundos': sg, 'postre': ps})
     except Exception as e:
         logging.exception("No se ha podido realizar la petición")
