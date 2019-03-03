c0176e4ca2334db30ea081b90bbba9aa92fbbbdc
arraiz <mikeldiezgarcia@gmail.com>
2019-03-03 18:03:04 +0100

    simple setup.py added

--- install-reqs.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-echo `pip3 list`
--- setup.py
+++ setup.py
@@ -1,1 +1,9 @@
 from distutils.core import setup
+
+setup(name='mainU',
+      version='1.0',
+      description='@description',
+      author='@authors',
+      author_email='dev@mainu.eus',
+      url='https://www.mainu.eus/',
+     )
